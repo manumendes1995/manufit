5743f2103779022af08b4055c656f8297aaf4780
manumendes1995 <manumendes199514@gmail.com>
2025-10-22 17:20:57 +0100

    🚀 Deploy completo 2025-10-22 16:20:57 UTC

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// Config geral
-export const APP_NAME = "Manufit";
-export const SUPPORT_EMAIL = "manumendes199514@gmail.com";
-export const PRICE_LABEL = "£39.99";
-
-// Stripe (se ainda não usas, deixa vazio por agora)
-export const STRIPE_PUBLISHABLE_KEY = "";
-export const STRIPE_PRICE_ID = "";
